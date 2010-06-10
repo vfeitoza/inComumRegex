33c66c3c01f6ee859bf1f067ca89d757b1853755
V. Feitoza <vfeitoza@gmail.com>
2010-06-10 18:13:57 -0300

    Domains redtube.com, pornhub.com and pornotube.com

--- src/inComum.cpp
+++ src/inComum.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <regex.h>
 #include <string>
+#include <vector>
 
 /*
  * BSD License
@@ -44,16 +46,20 @@ using namespace std;
 string get_var(const string url,const string var); //get some var from url
 string get_path(const string url, const char removeQuery); //path only (optionally remove query string)
 string get_domain(const string url); //get domain only (remove path)
-
+string regexMatch(string er, string line); //ER's
+void stringExplode(string str, string separator, vector<string>* results); //explode string
+string getFileName(string url); //get filename for url
 
 int main(int argc, char **argv)
 {
 	string url, urlf, id, domain;
 	string::size_type x;
 
-	if(argv[1][0] == '-' && argv[1][1] == 'v' ) {
-		cout << "inComum 0.3.2" << endl;
-		_exit(0);
+	if (argc > 1) {
+		if(argv[1][0] == '-' && argv[1][1] == 'v' ) {
+			cout << "inComum 0.3.2" << endl;
+			_exit(0);
+		}
 	}
 
 	while(getline(std::cin, url)){ // main loop receive all redirecting URL
@@ -118,6 +124,20 @@ int main(int argc, char **argv)
 		//msn catalog videos plugin
 		}else if(domain.find("catalog.video.msn.com/") != string::npos && url.find("share") != string::npos){
 			urlf = "http://catalog.msn.inComum/"+get_path(url,'N');
+
+		//redtube plugin
+		}else if((domain.find(".cdn.redtube.com") != string::npos) && 
+		         (regexMatch("videos-[0-9]{3,4}\\.cdn\\.redtube\\.com\\/s\\/[0-9]{7}\\/[a-zA-Z0-9]{9,12}\\.flv", url ) != "")){
+			urlf = "http://videos.redtube.inComum/"+ getFileName(url) +"?rs="+ get_var(url, "rs");
+
+		//pornhub plugin
+		}else if((regexMatch("nyc-v[0-9]{1,4}\\.pornhub\\.com\\/dl\\/", url ) != "") && 
+		         (regexMatch("(flv|mp4)", url ) != "") && (regexMatch("start=", url ) == "")){
+			urlf = "http://videos.pornhub.inComum/"+ getFileName(url);
+			
+		//pornotube plugin
+		}else if((regexMatch("video[0-9]{1,2}\\.pornotube\\.com\\/", url ) != "") && (regexMatch("(flv|mp4)", url ) != "")){
+			urlf = "http://videos.pornotube.inComum/"+ getFileName(url);
 
 		//else, return the same URL
 		}else{
@@ -192,3 +212,53 @@ string get_domain(const string url)
 
 }
 
+// usando ER's para facilitar a vida [risos]
+string regexMatch(string er, string line) 
+{
+    int error;
+    regmatch_t match;
+    regex_t reg;
+    if ((regcomp(&reg, er.c_str(), REG_EXTENDED | REG_NEWLINE)) == 0) {
+        error = regexec(&reg, line.c_str(), 1, &match, 0);
+        if (error == 0) {
+            return line.substr(match.rm_so, match.rm_eo - match.rm_so);
+        } else {
+            return "";
+        }
+    } else {
+        return "";
+    }
+}
+
+// quebramos a string em vetores (arrais)
+void stringExplode(string str, string separator, vector<string>* results)
+{
+    int found;
+    found = str.find_first_of(separator);
+    while(found != string::npos){
+        if(found > 0){
+            results->push_back(str.substr(0,found));
+        }
+        str = str.substr(found+1);
+        found = str.find_first_of(separator);
+    }
+    if(str.length() > 0){
+        results->push_back(str);
+    }
+}
+
+// pegarmos apenas o nome do arquivo
+string getFileName(string url)
+{
+    vector<string> resultado;
+    if (url.find("?") != string::npos) {
+        stringExplode(url, "?", &resultado);
+        stringExplode(resultado.at(resultado.size() - 2), "/", &resultado);
+
+        return resultado.at(resultado.size() -1);
+    } else {
+        stringExplode(url, "/", &resultado);
+
+        return resultado.at(resultado.size() -1);
+    }
+}
